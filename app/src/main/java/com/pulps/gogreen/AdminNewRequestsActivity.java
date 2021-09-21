added47dc36c900e057204c0d63516dd4b4e9740
Udithkanchana <87148738+Udithkanchana@users.noreply.github.com>
2021-09-21 14:08:00 +0530

    AdminNewRequestsActivity added with related java classes

---
+++ app/src/main/java/com/pulps/gogreen/AdminNewRequestsActivity.java
@@ -0,0 +1,14 @@
+package com.pulps.gogreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminNewRequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_new_requests);
+    }
+}
